1cf87f4552badfc9b5a96ba426c1670366de71a8
bils4307 <nuhhabil26@gmail.com>
2026-05-22 10:44:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈link gacor")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import datetime
+import streamlit as st
+
+d = st.date_input("When's your birthday", datetime.date(2007, 4, 3))
+st.write("Your birthday is:", bils)
