d21585a644148b8d9dfef5fc55fc8d52502fc165
bils4307 <nuhhabil26@gmail.com>
2026-05-22 10:44:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ st.write(
 import datetime
 import streamlit as st
 
-d = st.date_input("When's your birthday", datetime.date(2007, 4, 3))
+d = st.date_input("When's your birthday", datetime.date(2007, 3, 4))
 st.write("Your birthday is:", bils)
